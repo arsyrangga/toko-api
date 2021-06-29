a3eac25b8047d1547ca67090301343664b2645af
name <name@gmail.com>
2021-06-29 23:06:07 +0700

    rangga

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT;
 
+app.use(cors());
+app.use(express.json({ limit: "200mb" }));
+app.use(express.urlencoded({ extended: false }));
+
 sequelize
   .sync()
   .then(() => console.log("database terkonek"))
   .catch((err) => console.log(err));
 
-app.use(cors());
-app.use(express.json({ limit: "200mb" }));
-app.use(express.urlencoded({ extended: false }));
-
 app.use("/api", login);
 app.use("/api", loginAdmin);
 
-app.listen(8000, () => console.log("Terkonek di port " + PORT));
+app.listen(443, () => console.log("Terkonek di port " + PORT));
