0a1d623aa63acb0f77a8fba7abc68843ca077a2d
name <name@gmail.com>
2021-06-30 14:37:33 +0700

    rangga

--- routes/data_barang_routes.js
+++ routes/data_barang_routes.js
@@ -10,11 +10,22 @@ dataBarang.get("/data-barang", (req, res) => {
     .catch((err) => res.status(500).json(err));
 });
 
+// POST BARANG
 dataBarang.post("/data-barang-post", (req, res) => {
   const body = req.body;
   DataBarang.create(body)
     .then((result) => res.status(200).json(result))
     .catch((err) => res.status(500).json(err));
+});
+
+// Hapus Barang
+dataBarang.delete("/data-barang-delete/:id", (req, res) => {
+  const id = req.params.id;
+  DataBarang.findOne({
+    where: { id: id },
+  })
+    .then((result) => res.status(200).json(result))
+    .catch((err) => res.status(404).json(err));
 });
 
 module.exports = dataBarang;
